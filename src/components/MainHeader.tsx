cfbe0c1d51b25174912e1d5a4880b6dec9420263
Hushout <caoanhlamvn@gmail.com>
2024-04-28 11:04:50 +0200

    Creating Header

---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+interface MainHeaderProps {
+}
+
+const MainHeader:FC<MainHeaderProps> = () => {
+  return (
+    <div>MainHeader</div>
+  )
+}
+
+export default MainHeader

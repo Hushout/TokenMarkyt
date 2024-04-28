645c475d32cc237c42065e7d3c938af44101c391
Hushout <caoanhlamvn@gmail.com>
2024-04-28 11:48:52 +0200

    renaming

---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react'
+import { FaBars } from 'react-icons/fa'
+
+interface NavBarProps {
+}
+
+const NavBar: FC<NavBarProps> = () => {
+    const pages: { name: string; path: string }[] = [
+        { name: 'MARKET', path: '/market' },
+        { name: 'WALLET', path: '/wallet' },
+        { name: 'ABOUT', path: '/about' },
+    ]
+
+    const navLinks = pages.map((page, index) => {
+        return (
+            <a key={index} href={page.path} className='text-white font-bold px-2'>{page.name}</a>
+        )
+    })
+
+    return (
+        <div className='flex justify-between items-center w-100% px-4 h-auto'>
+            <a href='/'>Brand</a>
+            <div>
+                {/* <FaBars className='color-white cursor-pointer'/> */}
+                <nav role="navigation" className="flex">
+                    {navLinks}
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar

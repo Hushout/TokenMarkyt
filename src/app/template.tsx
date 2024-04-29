e3b2eaef11e4fc0c549325ab816dc8aea337e8ee
Hushout <caoanhlamvn@gmail.com>
2024-04-29 14:40:54 +0200

    api geckocoin to render Showcase Crypto

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,16 +2,20 @@
 import { animationPageIn } from '@/utils/animations';
 import React, { FC, useEffect } from 'react'
 
-const Template: FC = ({children} : {children: React.ReactNode}) => {
+interface TemplateProps {
+    children: React.ReactNode
+}
+
+const Template: FC<TemplateProps> = ({ children }: { children: React.ReactNode }) => {
     useEffect(() => {
         animationPageIn()
     }, [])
     return (
         <div>
-            <div id="banner1" className="min-h-screen bg-neutral-950 z-10 fixed top-0 left-[0%] w-1/4"></div>
-            <div id="banner2" className="min-h-screen bg-neutral-950 z-10 fixed top-0 left-[25%] w-1/4"></div>
-            <div id="banner3" className="min-h-screen bg-neutral-950 z-10 fixed top-0 left-[50%] w-1/4"></div>
-            <div id="banner4" className="min-h-screen bg-neutral-950 z-10 fixed top-0 left-[75%] w-1/4"></div>
+            <div id="banner1" className="min-h-screen bg-neutral-950 z-40 fixed top-0 left-[0%] w-1/4"></div>
+            <div id="banner2" className="min-h-screen bg-neutral-950 z-40 fixed top-0 left-[25%] w-1/4"></div>
+            <div id="banner3" className="min-h-screen bg-neutral-950 z-40 fixed top-0 left-[50%] w-1/4"></div>
+            <div id="banner4" className="min-h-screen bg-neutral-950 z-40 fixed top-0 left-[75%] w-1/4"></div>
             {children}
         </div>
     )
---
+++ src/pages/api/cryptos/index.tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+const apiKey = process.env.COINGECKO_API_KEY;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=8&x_cg_demo_api_key=${apiKey}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching cryptocurrency data:', error);
+    res.status(500).json({ message: 'Error fetching cryptocurrency data' });
+  }
+}

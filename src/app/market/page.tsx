36a3b0544f7aeadab0a935bc7b6da053ba8446e6
Hushout <caoanhlamvn@gmail.com>
2024-05-10 16:03:10 +0200

    market page search engine work

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,8 +1,54 @@
-import React, { FC } from 'react'
+'use client'
+import CardsShowcase, { Crypto } from '@/components/pages/homePage/CardsShowcase'
+import { SearchIcon } from '@/svg/Icon/SearchIcon'
+import { Input } from '@nextui-org/react'
+import React, { FC, useRef, useState } from 'react'
+import { useQuery } from "@tanstack/react-query";
+import debounce from 'lodash.debounce';
 
-const MarketPage:FC = () => {
+const MarketPage: FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const debouncedSearch = useRef(debounce(async (searchTerm: string) => {
+    const response = await fetch(`/api/cryptos/${searchTerm}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return response.json();
+  }, 500, { leading: true })).current;
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: ['searchCrypto', searchTerm],
+    queryFn: async () => {
+      const response = await debouncedSearch(searchTerm);
+      return response;
+    },
+    enabled: searchTerm !== '',
+  });
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const term = event.target.value;
+    setSearchTerm(term);
+    debouncedSearch(term);
+  };
+
   return (
-    <div>MarketPage</div>
+    <div className='flex flex-col items-center justify-center p-5 lg:p-20 mt-20'>
+      <Input
+        classNames={{
+          base: "max-w-full sm:max-w-[400px] h-10",
+          mainWrapper: "h-full",
+          input: "text-small",
+          inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+        }}
+        placeholder="Type to search..."
+        startContent={<SearchIcon size={18} />}
+        type="search"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      {!!data && !!data.coins ? <CardsShowcase cryptoList={data.coins} isLoading={isPending} isError={isError} /> : null}
+    </div>
   )
 }
 
--- src/components/pages/homePage/CardsShowcase.tsx
+++ src/components/pages/homePage/CardsShowcase.tsx
@@ -3,17 +3,17 @@ import React, { FC } from 'react';
 import { Card, CardBody, CardFooter, Image } from '@nextui-org/react';
 export interface Crypto {
     name: string;
-    image: string;
+    image?: string;
+    large?: string;
     current_price: number;
 }
 
 const CardsShowcase: FC<{ cryptoList?: Crypto[], isLoading?: Boolean, isError?: Boolean }> = ({ cryptoList, isLoading, isError }) => {
-
     if (isLoading) return <div>Loading...</div>;
     if (isError || !cryptoList) return <div>Error fetching data</div>;
 
     return (
-        <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+        <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 m-6">
             {cryptoList.map((item, index) => (
                 <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
                     <CardBody className="overflow-visible p-0">
@@ -23,7 +23,7 @@ const CardsShowcase: FC<{ cryptoList?: Crypto[], isLoading?: Boolean, isError?: 
                             width="100%"
                             alt={item.name}
                             className="w-full object-cover h-[140px]"
-                            src={item.image}
+                            src={!!item.image? item.image: item.large}
                         />
                     </CardBody>
                     <CardFooter className="text-small justify-between">
